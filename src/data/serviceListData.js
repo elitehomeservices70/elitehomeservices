e29bc82342a1671fa68c6f7b8bb0e3a4868b41d7
haseeb5890 <ahmedtele43421@gmail.com>
2025-09-20 10:51:59 +0500

    Updated service details page with new design

--- src/data/serviceListData.js
+++ src/data/serviceListData.js
@@ -5,6 +5,11 @@ const serviceListData = [
     title: "Garage Door Repair Services",
     img: "../repairMain.jpg",
   },
+  {
+    id: "new-services",
+    title: "New Services",
+    img: "../repairMain.jpg",
+  },
 ];
 
 export default serviceListData;
