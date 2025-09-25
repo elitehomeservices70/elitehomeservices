1e500f966859a705328c236583233a9e6553c436
Bilal Khan <bk3210206@gmail.com>
2025-09-26 01:54:48 +0500

    UI changes

--- src/data/serviceDetailsData.js
+++ src/data/serviceDetailsData.js
@@ -6,86 +6,86 @@ const serviceDetailsData = {
   garage_door_repair_services: [
     {
       description:
-        "Get expert garage door repair and maintenance services from Elite Home Services. Our skilled technicians fix broken springs, faulty openers, damaged panels, and misaligned tracks, ensuring smooth and secure operation. With prompt, reliable, and professional service, we restore functionality and boost curb appeal. Trust us for top-quality garage door solutions, from quick fixes to full replacements. Contact us today for a hassle-free experience!",
+        "Get expert garage door repair and maintenance services from Elite Home Services Experts. Our skilled technicians fix broken springs, faulty openers, damaged panels, and misaligned tracks, ensuring smooth and secure operation. With prompt, reliable, and professional service, we restore functionality and boost curb appeal. Trust us for top-quality garage door solutions, from quick fixes to full replacements. Contact us today for a hassle-free experience!",
     },
     {
       title: "♦️REPAIR OF COMMERCIAL GARAGE DOORS",
       description:
-        "Elite Home Services provides expert commercial garage door repair services to minimize downtime and maximize productivity. Our skilled technicians quickly diagnose and fix issues with worn-out parts, complex system failures, and more, getting your business back up and running. With prompt and reliable service, we ensure your commercial garage door operates smoothly, reducing disruptions and enhancing business efficiency. Trust us for fast and effective repairs, and get your business back on track.",
+        "Elite Home Services Experts provides expert commercial garage door repair services to minimize downtime and maximize productivity. Our skilled technicians quickly diagnose and fix issues with worn-out parts, complex system failures, and more, getting your business back up and running. With prompt and reliable service, we ensure your commercial garage door operates smoothly, reducing disruptions and enhancing business efficiency. Trust us for fast and effective repairs, and get your business back on track.",
       img: "/repair.jpg",
     },
     {
       title: "♦️INSTALLATION OF RESIDENTIAL GARAGE DOORS",
       description:
-        "Choosing the right garage door for your home involves considering several key factors. You can select from two main types: sectional panel doors, a classic and popular choice, or accordion-style carriage doors, offering a unique design. After deciding on the door style, consider insulation options to enhance energy efficiency and durability. At Elite Home Services, we can help you navigate the selection process and find the perfect garage door for your needs.",
+        "Choosing the right garage door for your home involves considering several key factors. You can select from two main types: sectional panel doors, a classic and popular choice, or accordion-style carriage doors, offering a unique design. After deciding on the door style, consider insulation options to enhance energy efficiency and durability. At Elite Home Services Experts, we can help you navigate the selection process and find the perfect garage door for your needs.",
       img: "/installation.jpg",
     },
     {
       title: "♦️GARAGE CABLE REPAIRS",
       description:
-        "Ensure your garage door operates smoothly and safely with Elite Home Services' expert cable inspection and repair services. Our technicians provide thorough inspections to prevent unexpected failures, identifying worn-out or damaged cables that can cause system breakdowns. Contact us today for prompt and reliable cable repair services, minimizing downtime and enhancing garage door safety.",
+        "Ensure your garage door operates smoothly and safely with Elite Home Services Experts' expert cable inspection and repair services. Our technicians provide thorough inspections to prevent unexpected failures, identifying worn-out or damaged cables that can cause system breakdowns. Contact us today for prompt and reliable cable repair services, minimizing downtime and enhancing garage door safety.",
       img: "/cable.jpg",
     },
     {
       title: "♦️AUTOMATIC DOOR OPENER",
       description:
-        "Automatic garage door openers can malfunction at the most inconvenient times, causing frustration for homeowners and business owners alike. Despite being a common repair, the unexpected failure of these systems can disrupt daily routines and operations. At Elite Home Services, we specialize in prompt and reliable garage door opener replacement and repair, ensuring your door operates smoothly and efficiently when you need it most.",
+        "Automatic garage door openers can malfunction at the most inconvenient times, causing frustration for homeowners and business owners alike. Despite being a common repair, the unexpected failure of these systems can disrupt daily routines and operations. At Elite Home Services Experts, we specialize in prompt and reliable garage door opener replacement and repair, ensuring your door operates smoothly and efficiently when you need it most.",
       img: "/autodoor.jpg",
     },
     {
       title: "♦️BROKEN SPRINGS",
       description:
-        "Garage doors depend on springs, cables, and openers to function correctly. Springs typically last 5 years, wearing out with use. Regular maintenance prevents unexpected failures, ensuring safe operation. Elite Home Services experts maintain and repair garage door systems for optimal performance, extending lifespan and safety.",
+        "Garage doors depend on springs, cables, and openers to function correctly. Springs typically last 5 years, wearing out with use. Regular maintenance prevents unexpected failures, ensuring safe operation. Elite Home Services Experts maintain and repair garage door systems for optimal performance, extending lifespan and safety.",
       img: "/brokensprings.jpg",
     },
     {
       title: "♦️GLASS DOOR REPAIRS",
       description:
-        "Damaged or malfunctioning glass doors can compromise the security and aesthetic appeal of your home or business. At Elite Home Services, our experts provide professional glass door repair services to restore functionality and beauty. From shattered glass replacement to faulty mechanism repair, we identify and fix problems efficiently. Contact us today for reliable glass door repair solutions that ensure safe and smooth operation.",
+        "Damaged or malfunctioning glass doors can compromise the security and aesthetic appeal of your home or business. At Elite Home Services Experts, our experts provide professional glass door repair services to restore functionality and beauty. From shattered glass replacement to faulty mechanism repair, we identify and fix problems efficiently. Contact us today for reliable glass door repair solutions that ensure safe and smooth operation.",
       img: "/glassdoor.jpg",
     },
     {
       title: "♦️GARAGE DOOR WHEEL REPAIR",
       description:
-        "A broken garage door wheel can be a major inconvenience, causing noise, irritation, and disruption. If left unchecked, it can lead to further damage and wear on your garage door system. Knowing where to look for garage door wheels and addressing issues promptly is crucial. At Elite Home Services, we can help you identify and fix problems with your garage door wheels, ensuring smooth and safe operation.",
+        "A broken garage door wheel can be a major inconvenience, causing noise, irritation, and disruption. If left unchecked, it can lead to further damage and wear on your garage door system. Knowing where to look for garage door wheels and addressing issues promptly is crucial. At Elite Home Services Experts, we can help you identify and fix problems with your garage door wheels, ensuring smooth and safe operation.",
       img: "/brokensprings.jpg",
     },
     {
       title: "♦️TRACK REPAIRS",
       description:
-        "Damaged or misaligned garage door tracks can cause frustrating issues with door operation. At Elite Home Services, our experts provide prompt and reliable track repair services to get your garage door running smoothly. We identify and fix track problems, ensuring safe and efficient door operation. Contact us today for professional track repair solutions.",
+        "Damaged or misaligned garage door tracks can cause frustrating issues with door operation. At Elite Home Services Experts, our experts provide prompt and reliable track repair services to get your garage door running smoothly. We identify and fix track problems, ensuring safe and efficient door operation. Contact us today for professional track repair solutions.",
       img: "/trackrepairs.jpg",
     },
   ],
   air_duct_and_chimney_cleaning_services: [
     {
       description:
-        "Elite Home Services offers comprehensive solutions for a healthier and safer home. Our expert technicians provide professional air duct cleaning services to remove dust, debris, and allergens, improving indoor air quality and HVAC efficiency. We also specialize in chimney cleaning and maintenance, thoroughly inspecting and cleaning chimneys, fireplaces, and vents to prevent creosote buildup and potential hazards. Contact us today to schedule your air duct and chimney cleaning services and enjoy a cleaner, healthier, and more comfortable living space.",
+        "Elite Home Services Experts offer comprehensive solutions for a healthier and safer home. Our expert technicians provide professional air duct cleaning services to remove dust, debris, and allergens, improving indoor air quality and HVAC efficiency. We also specialize in chimney cleaning and maintenance, thoroughly inspecting and cleaning chimneys, fireplaces, and vents to prevent creosote buildup and potential hazards. Contact us today to schedule your air duct and chimney cleaning services and enjoy a cleaner, healthier, and more comfortable living space.",
       img: "/Admain.jpg",
     },
     {
       title: "♦️AIR DUCT REPAIRS",
       description:
-        "Don't let damaged or leaky air ducts compromise your home's comfort and efficiency. Elite Home Services offers expert air duct repair services to seal leaks, fix damaged sections, and restore your HVAC system's performance. Our skilled technicians use high-quality materials and advanced techniques to ensure durable repairs that improve indoor air quality and reduce energy bills. Contact us today to schedule your air duct repair service and breathe easy knowing your system is working optimally.",
+        "Don't let damaged or leaky air ducts compromise your home's comfort and efficiency. Elite Home Services Experts offer expert air duct repair services to seal leaks, fix damaged sections, and restore your HVAC system's performance. Our skilled technicians use high-quality materials and advanced techniques to ensure durable repairs that improve indoor air quality and reduce energy bills. Contact us today to schedule your air duct repair service and breathe easy knowing your system is working optimally.",
 
       img: "/airductrepair.jpg",
     },
     {
       title: "♦️AIR DUCT INSTALLATION",
       description:
-        "Upgrade your home's comfort and efficiency with Elite Home Services' expert air duct installation services. Our skilled technicians design and install custom air duct systems that optimize airflow, reduce energy bills, and improve indoor air quality. From design to installation, we ensure a seamless process that meets your specific needs and budget. Contact us today to schedule your air duct installation service and enjoy a healthier, more comfortable living space.",
+        "Upgrade your home's comfort and efficiency with Elite Home Services Experts' expert air duct installation services. Our skilled technicians design and install custom air duct systems that optimize airflow, reduce energy bills, and improve indoor air quality. From design to installation, we ensure a seamless process that meets your specific needs and budget. Contact us today to schedule your air duct installation service and enjoy a healthier, more comfortable living space.",
       img: "/airductinstallation.jpg",
     },
     {
       title: "♦️DRYER VENTS",
       description:
-        "Improve your home's safety and efficiency with Elite Home Services' expert dryer vent cleaning and repair services. Our skilled technicians remove lint buildup, debris, and blockages from your dryer vents, reducing the risk of fires and improving drying times. With our professional dryer vent services, you can enjoy peace of mind, lower energy bills, and extended appliance lifespan. Contact us today to schedule your dryer vent cleaning and repair service.",
+        "Improve your home's safety and efficiency with Elite Home Services Experts' expert dryer vent cleaning and repair services. Our skilled technicians remove lint buildup, debris, and blockages from your dryer vents, reducing the risk of fires and improving drying times. With our professional dryer vent services, you can enjoy peace of mind, lower energy bills, and extended appliance lifespan. Contact us today to schedule your dryer vent cleaning and repair service.",
       img: "/dryer-vent.jpg",
     },
     {
       title: "♦️CHIMNEY CLEANING",
       description:
-        "Keep your home safe and warm with Elite Home Services' professional chimney cleaning services. Our experienced technicians thoroughly clean and inspect chimneys, fireplaces, and vents to remove creosote buildup, soot, and debris, reducing the risk of chimney fires and carbon monoxide poisoning. With our expert chimney cleaning services, you can enjoy a cozy, worry-free winter. Contact us today to schedule your chimney cleaning service.",
+        "Keep your home safe and warm with Elite Home Services Experts' professional chimney cleaning services. Our experienced technicians thoroughly clean and inspect chimneys, fireplaces, and vents to remove creosote buildup, soot, and debris, reducing the risk of chimney fires and carbon monoxide poisoning. With our expert chimney cleaning services, you can enjoy a cozy, worry-free winter. Contact us today to schedule your chimney cleaning service.",
       img: "/chimneycleaning.jpg",
     },
   ],
